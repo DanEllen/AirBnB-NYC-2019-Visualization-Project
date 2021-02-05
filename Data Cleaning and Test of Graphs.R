be75d28bf44b3534cf33fcd260c1c3c7ee12fc7e
DanEllen <danielellenbogen@gmail.com>
2021-02-05 11:53:58 -0500

    Added Treemap graph
    For listings/price per neighbourhood

--- Data Cleaning and Test of Graphs.R
+++ Data Cleaning and Test of Graphs.R
@@ -7,21 +7,24 @@ names(airbnb_data)
 str(airbnb_data)
 summary(airbnb_data)
 
-#Objective.	Map with options to see density (heat map) based on
-#number of listings, price, and reviews per month. 
+##############################################
+#About libraries used
 
-#Leaflet is one of the most popular open-source JavaScript libraries for 
+library(leaflet)#Leaflet is one of the most popular open-source JavaScript libraries for 
 #interactive maps. This leaflet R package makes it easy to integrate and control
 #Leaflet maps in R. 
-#leaflet.extras package provides a simple heat map function, addheatmap().
+library(leaflet.extras) #Provides a simple heat map function, addheatmap().
+library(tidyverse) #Several tools to organize data
+library(ggplot2) #Several tools for graphing
+library(treemapify) # Allows easy creation of Treemap graph
+library(scales) #Allows easily labeling of axis. Example:
+#scale_x_continuous(labels = unit_format(unit = "K", scale = 1e-3))
+library(lubridate) #Allows easy extraction of day, month, year
 
 
-
-install.packages("leaflet")
-install.packages('leaflet.extras')
-
-library(leaflet)
-library(leaflet.extras)
+##############################################
+#1 Map with options to see density (heat map) based number of listings, 
+#price per night, and reviews per month. 
 
 #Base map
 
@@ -39,7 +42,7 @@ map  %>%
     cellSize = 7,
     radius = 5
   )  %>%
-  addMarkers( #Adds markets in clusters
+  addMarkers( #Adds markets in clusters. Have this as an option in Shiny
     lng = airbnb_data$longitude,
     lat = airbnb_data$latitude,
     label = airbnb_data$host_id,
@@ -76,3 +79,34 @@ map  %>%
     cellSize = 2,
     radius = 2
   )
+
+##############################################
+#2.	Graphs for composition by neighborhood comparing number of listings, 
+#price per night, and reviews per month. 
+
+#Treemap graph for number of listing comparing neighborhood_group and
+#neighboorhood
+
+#Organize the data
+
+treemap_data = airbnb_data %>% 
+  group_by(neighbourhood, neighbourhood_group) %>% 
+  summarize(total_listings = n(), avg_price = mean(price))
+
+#Plot the graph
+
+ggplot(treemap_data, aes(area = total_listings, fill = avg_price,
+                         label = neighbourhood,
+                         subgroup = neighbourhood_group)) +
+  geom_treemap() +
+  scale_alpha_continuous(range = c(0.2, 1)) +
+  geom_treemap_subgroup_border() +
+  geom_treemap_subgroup_text(place = "centre", grow = T, alpha = 0.6, colour =
+                               "black", fontface = "italic", min.size = 0) +
+  geom_treemap_text(colour = "grey78", place = "topleft", reflow = T) +
+  scale_fill_gradient2(low="white", mid="yellow", high="red", midpoint = 170,
+                       limits = c(80, 400), oob = scales::squish) +
+  labs(fill = "Average Price")
+  
+  
+
