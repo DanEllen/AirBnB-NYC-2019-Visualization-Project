491ffa283b43125d060a76ef096eb6c4552ebcc1
DanEllen <danielellenbogen@gmail.com>
2021-02-20 18:12:18 -0500

    Comented out a line to allow shinyapps.io to work

--- global.R
+++ global.R
@@ -1,6 +1,6 @@
 #Setting the directory to where the data is located
 
-setwd("C:/Users/Daniel/Dropbox/Data Science/NYC DSA/R/Exploratory Visualization and Shiny Project/AirBnB-NYC-2019-Visualization-Project")
+#setwd("C:/Users/Daniel/Dropbox/Data Science/NYC DSA/R/Exploratory Visualization and Shiny Project/AirBnB-NYC-2019-Visualization-Project")
 
 #Loading the necesary data frames
 
